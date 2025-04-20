b7673112930f5004e9b89218020dbd544da2a06e
NetAcad DEVASC <changeme@example.com>
2025-04-20 20:14:55 +0000

    Desarrollo de actividad 1 por [Nombre del estudiante]

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Este es un archivo de prueba para la evaluación" > archivo_prueba.txt
+
+
+chmod 744 archivo_prueba.txt
+
+
+sudo chown root archivo_prueba.txt
+
+
+echo "Archivo creado, permisos y propietario asignados."
+
+
+
+
